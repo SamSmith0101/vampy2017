d2b80dc2693cdbe880adf048c8a9396cab3c725c
root <root@vampy.cs>
2017-06-20 21:29:37 -0500

    a thing

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+dnf update -y
+dnf remove gnome-boxes gnome-contacts gnome-maps gnome-weather libreoffice-* evolution rhythmbox firefox -y
+pip3 install --upgrade pip
+dnf install python3-tkinter
+cat res/user > /home/vampy/.config/dconf/user
