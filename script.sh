7cd2dfc6776155e5d15e8b59553c9143492e8b35
x <x@x.com>
2017-07-02 11:53:11 -0500

    shut up

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ dnf remove gnome-boxes gnome-contacts gnome-maps gnome-weather gnome-software li
 dnf update -y
 pip3 install --upgrade pip
 dnf install python3-tkinter jre java-devel nano -y
-cat res/user > /home/vampy/.config/dconf/user
+#cat res/user > /home/vampy/.config/dconf/user
+echo "alias espeak='echo With great power comes great responsibility.'" >> /home/vampy/.bash_rc
