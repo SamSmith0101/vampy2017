ff9e6365dbb0169ac80224a063d65be893521f53
x <x@x.com>
2017-07-03 07:57:11 -0500

    no sound

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-dnf remove gnome-boxes gnome-contacts gnome-maps gnome-weather gnome-software libreoffice-* evolution rhythmbox firefox -y
-dnf update -y
-pip3 install --upgrade pip
-dnf install python3-tkinter jre java-devel nano -y
+#dnf remove gnome-boxes gnome-contacts gnome-maps gnome-weather gnome-software libreoffice-* evolution rhythmbox firefox -y
+#dnf update -y
+#pip3 install --upgrade pip
+#dnf install python3-tkinter jre java-devel nano -y
 #cat res/user > /home/vampy/.config/dconf/user
 echo "alias espeak='echo With great power comes great responsibility.'" >> /home/vampy/.bashrc
+echo "blacklist snd_hda_intel" > /etc/modprobe.d/sound.blacklist.conf
