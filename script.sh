847f9877c7f92471ba4af4d205e0576b4304f9d2
x <x@x.com>
2017-07-11 07:55:49 -0500

    game

--- script.sh
+++ script.sh
@@ -6,5 +6,6 @@
 #dnf install python3-tkinter jre java-devel nano -y
 #cat res/user > /home/vampy/.config/dconf/user
 #echo "alias espeak='echo With great power comes great responsibility.'" >> /home/vampy/.bashrc
-echo "blacklist snd_hda_intel" > /etc/modprobe.d/sound.blacklist.conf
-shutdown -r now
+#echo "blacklist snd_hda_intel" > /etc/modprobe.d/sound.blacklist.conf
+#shutdown -r now
+pip install pygame
